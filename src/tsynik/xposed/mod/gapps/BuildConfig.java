96b16d8b6ecd5afb43024bcf9ab88415bffabbf0
nikk gitanes <tsynik@gmail.com>
2020-03-14 06:52:24 +0300

    version bump

--- src/tsynik/xposed/mod/gapps/BuildConfig.java
+++ src/tsynik/xposed/mod/gapps/BuildConfig.java
@@ -12,6 +12,6 @@ public final class BuildConfig {
     public static final String APPLICATION_ID = "tsynik.xposed.mod.gapps";
     public static final String BUILD_TYPE = "release";
     public static final String FLAVOR = "";
-    public static final int VERSION_CODE = 2;
-    public static final String VERSION_NAME = "1.2";
+    public static final int VERSION_CODE = 3;
+    public static final String VERSION_NAME = "1.3";
 }
